555d9db14d884ad4bb4b360deb75204287d16d07
Vadim Kadnikov <vadimka@scribd.com>
2024-04-04 15:24:08 +0200

    feat: Add delete_in argument

--- variables.tf
+++ variables.tf
@@ -21,6 +21,18 @@ variable "secrets" {
   )
 }
 
+variable "delete_in" {
+  description = "Number of days to wait before secret deletion"
+  type        = number
+
+  default = 30
+
+  validation {
+    condition     = var.delete_in == 0 || contains(range(7, 30), var.delete_in)
+    error_message = "The delete_in value must be 0 or between 7 and 30."
+  }
+}
+
 variable "tags" {
   description = "Key-value map of tags"
   type        = map(string)
